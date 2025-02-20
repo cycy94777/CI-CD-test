46f51a98c65e5b3590860416a7132aeef78566cf
楊思婷 <yangsiting@christines-MacBook-Air.local>
2025-02-20 09:58:05 +0800

    測試錯誤

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,5 +7,5 @@ from my_app import add  # 這裡應該要成功找到 my_app.py
 
 
 def test_add():
-    assert add(2, 3) == 5 #預期 2 + 3 = 5
+    assert add(2, 3) == 4 #預期 2 + 3 = 5
     assert add(-1, 1) == 0 #預期 -1 + 1 = 0
